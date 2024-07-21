c087ea08a2e941aa915fcab2f8ef9970cfc26aa5
Alexander Williamson <alex@alexw.co.uk>
2024-07-22 00:20:03 +0100

    Fix deploy

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   env: {
     BUILD_TIME: new Date().toISOString(),
   },
+  output: "export"
 };
+
+module.exports = nextConfig
